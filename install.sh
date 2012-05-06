ee373f4c0f3d5d2fc8ee27e462066778df2a6251
Sindre Wetjen <sindre.w@gmail.com>
2012-05-06 16:08:32 +0200

    Making install more intelligent. Can now reinstall without removing directories first.

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# -*- coding: utf-8 -*-
+
+INSTALL_DIRECTORY=$HOME/.submacs/
+DOTEMACS=$HOME/.emacs
+PYTHON_MODE_VERSION=6.0.5
+HYPER_SPEC_VERSION=7-0
+MAGIT_VERSION=1.1.1
 
 if [ ! -f /usr/bin/wget ]; then
     echo "Please install wget"
@@ -14,65 +21,107 @@ echo "Disclaimer: Packages and programs downloaded under install"
 echo "have individual licenses. Please look at each individual "
 echo "packages homesite for information."
 
-INSTALL_DIRECTORY=$HOME/.submacs/
-DOTEMACS=$HOME/.emacs
-PYTHON_MODE_VERSION=6.0.5
-HYPER_SPEC_VERSION=7-0
-MAGIT_VERSION=1.1.1
+function overwrite_dotemacs() {
+    if [ -f $DOTEMACS ]; then
+	echo "NB: Backing up .emacs to .emacs-bck"
+	mv $DOTEMACS ${DOTEMACS}-bck
+    fi
 
-if [ ! -d $INSTALL_DIRECTORY ]; then
-    mkdir $INSTALL_DIRECTORY
-fi
+    touch ${DOTEMACS}
+    echo ";; -*- coding: utf-8 -*-" >> ${DOTEMACS}
+    echo ";; 'The modern world uses UTF-8. Emacs is modern." >> ${DOTEMACS}
+    echo ";; Therefore emacs uses UTF-8.' -- Johanbev" >> ${DOTEMACS}
 
-cp src/*.el $INSTALL_DIRECTORY
-cp README $INSTALL_DIRECTORY
-cd $INSTALL_DIRECTORY
+    echo "(prefer-coding-system 'utf-8)" >> ${DOTEMACS}
+    echo '(set-language-environment "UTF-8")' >> ${DOTEMACS}
 
-echo "Fetching Python Mode"
-wget https://launchpad.net/python-mode/trunk/$PYTHON_MODE_VERSION/+download/python-mode.el-$PYTHON_MODE_VERSION.tar.gz &> /dev/null
-tar -xf python-mode.el-$PYTHON_MODE_VERSION.tar.gz
-mv python-mode.el-$PYTHON_MODE_VERSION python-mode
-rm python-mode.el-$PYTHON_MODE_VERSION.tar.gz
+    echo "(defvar install_directory \"${INSTALL_DIRECTORY}\")" >> ${DOTEMACS}
+    echo "(add-to-list 'load-path install_directory)" >> ${DOTEMACS}
+    echo "(require 'full-setup)" >> ${DOTEMACS}
+}
 
-echo "Fetching Slime"
-wget http://common-lisp.net/project/slime/snapshots/slime-current.tgz &> /dev/null
-tar -xf slime-current.tgz
-mv slime-2* slime
-rm slime-current.tgz
+function clean() {
+    if [ $INSTALL_DIRECTORY == $HOME -o $INSTALL_DIRECTORY == / ]; then
+	echo "Listen: you don't want to clean your home directory or your root directory."
+	exit 1;
+    fi
+    
+    echo -ne "Are you sure you want to completely remove \"$INSTALL_DIRECTORY\" [y/N]? "
+    read line
+    if [ -z $line ]; then exit 0; fi
+    if [ $line == y -o $line == Y ]; then
+	if [ -d $INSTALL_DIRECTORY ]; then
+	    rm -rf $INSTALL_DIRECTORY
+	fi
+    else
+	exit 0;
+    fi
+}
 
-echo "Fetching HyperSpec"
-mkdir HyperSpec
-cd HyperSpec
-wget ftp://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-$HYPER_SPEC_VERSION.tar.gz &> /dev/null
-tar -xf HyperSpec-$HYPER_SPEC_VERSION.tar.gz
-rm HyperSpec-$HYPER_SPEC_VERSION.tar.gz
-cd ..
+function install() {
+    if [ ! -d $INSTALL_DIRECTORY ]; then
+	mkdir $INSTALL_DIRECTORY
+    fi
 
-echo "Fetching Magit"
-wget https://github.com/downloads/magit/magit/magit-$MAGIT_VERSION.tar.gz &> /dev/null
-tar -xf magit-$MAGIT_VERSION.tar.gz 
-mv magit-$MAGIT_VERSION magit
-rm magit-$MAGIT_VERSION.tar.gz
+    cp src/*.el $INSTALL_DIRECTORY
+    cp README $INSTALL_DIRECTORY
+    cd $INSTALL_DIRECTORY
 
-echo "Fetching Single files"
-mkdir single-files
-cd single-files
-wget http://dishevelled.net/elisp/lambda-mode.el &> /dev/null
-cd ..
+    echo "Fetching Python Mode"
+    wget https://launchpad.net/python-mode/trunk/$PYTHON_MODE_VERSION/+download/python-mode.el-$PYTHON_MODE_VERSION.tar.gz &> /dev/null
+    tar -xf python-mode.el-$PYTHON_MODE_VERSION.tar.gz
+    mv python-mode.el-$PYTHON_MODE_VERSION python-mode
+    rm python-mode.el-$PYTHON_MODE_VERSION.tar.gz
 
-if [ -f $DOTEMACS ]; then
-    echo "NB: Backing up .emacs to .emacs-bck"
-    mv $DOTEMACS ${DOTEMACS}-bck
-fi
+    echo "Fetching Slime"
+    wget http://common-lisp.net/project/slime/snapshots/slime-current.tgz &> /dev/null
+    tar -xf slime-current.tgz
+    mv slime-2* slime
+    rm slime-current.tgz
 
-touch ${DOTEMACS}
-echo ";; -*- coding: utf-8 -*-" >> ${DOTEMACS}
-echo ";; 'The modern world uses UTF-8. Emacs is modern." >> ${DOTEMACS}
-echo ";; Therefore emacs uses UTF-8.' -- Johanbev" >> ${DOTEMACS}
+    echo "Fetching HyperSpec"
+    mkdir HyperSpec
+    cd HyperSpec
+    wget ftp://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-$HYPER_SPEC_VERSION.tar.gz &> /dev/null
+    tar -xf HyperSpec-$HYPER_SPEC_VERSION.tar.gz
+    rm HyperSpec-$HYPER_SPEC_VERSION.tar.gz
+    cd ..
 
-echo "(prefer-coding-system 'utf-8)" >> ${DOTEMACS}
-echo '(set-language-environment "UTF-8")' >> ${DOTEMACS}
+    echo "Fetching Magit"
+    wget https://github.com/downloads/magit/magit/magit-$MAGIT_VERSION.tar.gz &> /dev/null
+    tar -xf magit-$MAGIT_VERSION.tar.gz 
+    mv magit-$MAGIT_VERSION magit
+    rm magit-$MAGIT_VERSION.tar.gz
 
-echo "(defvar install_directory \"${INSTALL_DIRECTORY}\")" >> ${DOTEMACS}
-echo "(add-to-list 'load-path install_directory)" >> ${DOTEMACS}
-echo "(require 'full-setup)" >> ${DOTEMACS}
+    echo "Fetching Single files"
+    mkdir single-files
+    cd single-files
+    wget http://dishevelled.net/elisp/lambda-mode.el &> /dev/null
+    cd ..
+}
+
+function newinstall() {
+    install
+    overwrite_dotemacs
+}
+
+function reinstall() {
+    clean
+    install
+}
+
+OPT=NEWINSTALL
+
+while getopts “r” OPTION
+do
+    case $OPTION in
+	r) OPT=REINSTALL;;
+    esac
+done
+
+case $OPT in
+    NEWINSTALL) 
+	newinstall;;
+    REINSTALL) 
+	reinstall;;
+esac
