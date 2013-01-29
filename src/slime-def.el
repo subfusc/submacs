7783cd8dab0ec9d47224549a1caf8e3af7ef3227
Sindre Wetjen <sindre.w@gmail.com>
2013-01-29 12:34:50 +0100

    Updating slimedef to include autodoc and slime repl

--- src/slime-def.el
+++ src/slime-def.el
@@ -9,29 +9,48 @@
 (add-to-list 'load-path slime-path)
 
 (require 'slime)
+;(require 'slime-autoloads)
+(require 'ac-slime)
 (require 'hyperspec)
-(slime-setup)
+
+;(autoload 'slime "my-slime" "Slime mode." t)
+;(autoload 'slime-connect "my-slime" "Slime mode." t)
+
+(slime-setup '(slime-asdf
+               slime-autodoc
+               slime-editing-commands
+               slime-fancy
+               slime-fontifying-fu
+               slime-fuzzy
+               slime-indentation
+               slime-mdot-fu
+               slime-package-fu
+               slime-references
+               slime-repl
+               slime-sbcl-exts
+               slime-scratch
+               slime-xref-browser))
 
 (eval-after-load "slime" 
   '(progn
     (setq common-lisp-hyperspec-root (format "%s%s" install_directory "/HyperSpec/HyperSpec/"))
-    ;; (add-hook 'slime-mode-hook 
-    ;; (lambda ()
-    ;;   (set-variable lisp-indent-function 'common-lisp-indent-function)
-    ;;   (local-set-key [tab] 'slime-indent-and-complete-symbol)))
     (add-hook 'inferior-lisp-mode-hook 
-     (lambda () 
-       (inferior-slime-mode t)))))
+              (lambda () 
+                (inferior-slime-mode t)))))
+
+;; (add-hook 'slime-mode-hook 
+;; (lambda ()
+;;   (set-variable lisp-indent-function 'common-lisp-indent-function)
+;;   (local-set-key [tab] 'slime-indent-and-complete-symbol)))
 ;; (add-hook 'inferior-slime-mode-hook
 ;;  (lambda ()
 ;;    (local-set-key [tab] 'slime-indent-and-complete-symbol)))))
   
 (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+;(add-hook 'lisp-mode-hook (lambda () (slime-autodoc-mode t)))
 (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
-
-(require 'ac-slime)
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-repl-mode-hook 'set-up-slime-a)
+;(add-hook 'slime-repl-mode-hook 'set-up-slime-a)
 
 (global-set-key [f5] 'slime)
 ;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
