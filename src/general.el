b4eb601bbfac17d5cf97afa368fda4520f1c5935
Sindre Wetjen <sindre.w@gmail.com>
2018-01-06 10:22:31 +0100

    Adding keybindings for multiple-cursor modes

--- src/general.el
+++ src/general.el
@@ -16,5 +16,10 @@
 	try-expand-whole-kill))
 
 (global-set-key [(meta /)] 'hippie-expand)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 
 (provide 'general)
