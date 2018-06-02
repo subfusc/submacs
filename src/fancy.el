2aaa5dd4718fcda843ae5d40583670d1e8685ec7
Sindre Wetjen <sindre@tjenen.de>
2018-06-02 19:23:33 +0200

    [Global] Use emacs built-in features to create pretty lambdas

--- src/fancy.el
+++ src/fancy.el
@@ -1,4 +1,12 @@
-(global-pretty-lambda-mode)
+(defun my-pretty-lambda ()
+  "make some word or string show as pretty Unicode symbols"
+  (setq prettify-symbols-alist
+        '(
+          ("lambda" . 955) ; λ
+          ))
+(add-hook 'scheme-mode-hook 'my-pretty-lambda)
+(global-prettify-symbols-mode 1)
+
 (ido-mode)
 
 (provide 'fancy)
