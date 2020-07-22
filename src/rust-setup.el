ae9a62eed9349fa678760e43fc890658f53fbe68
Sindre Wetjen <git@tjenen.de>
2020-07-23 00:17:53 +0200

    [Rust] Manually add the rustup paths in case its not in env

--- src/rust-setup.el
+++ src/rust-setup.el
@@ -1,12 +1,19 @@
 ;; -*- coding: utf-8 -*-
 (require 'rust-mode)
 
+(extend-emacs-executable-paths (concat (getenv "HOME") "/.cargo/bin"))
+(setq racer-rust-src-path
+      (concat (string-trim (shell-command-to-string "rustc --print sysroot"))
+	      "/lib/rustlib/src/rust/src"))
+
+
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 (add-hook 'rust-mode-hook #'whitespace-mode)
 
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(define-key rust-mode-map (kbd "C-c C-r") #'rust-run)
 (setq company-tooltip-align-annotations t)
 
 (provide 'rust-setup)
