d215f52c2dbd1cd4fcbfd7d8f1b2ee916d1bff54
Sindre Wetjen <git@tjenen.de>
2022-11-04 23:54:45 +0100

    Change theme. Tsdh-dark didn't play well with dart lsp

--- src/looks.el
+++ src/looks.el
@@ -12,6 +12,6 @@
 (custom-set-variables
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (tsdh-dark))))
+ '(custom-enabled-themes (quote (wombat))))
 
 (provide 'looks)
