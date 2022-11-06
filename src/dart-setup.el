694216028879325b7da7c9d74af9bde639df5181
Sindre Wetjen <git@tjenen.de>
2022-11-06 03:41:41 +0100

    Do not use the lsp auto-formatter

--- src/dart-setup.el
+++ src/dart-setup.el
@@ -1,5 +1,10 @@
 (require 'dart-mode)
 
+;; The auto formater for dart is really not god IMO.
+(custom-set-variables
+ '(lsp-dart-enable-sdk-formatter nil)
+ '(lsp-enable-on-type-formatting nil))
+
 (add-hook 'dart-mode-hook 'lsp)
 
 (setq gc-cons-threshold (* 100 1024 1024)
