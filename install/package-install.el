45ff5ec70e99a5e6b1729cd52d9098d85c01fc4d
Sindre <git@tjenen.de>
2023-07-25 17:40:36 +0000

    [Docker,Whitespace] Add dockerfile mode
    Add the dockerfile mode and indent properly the wanted packages

--- install/package-install.el
+++ install/package-install.el
@@ -6,23 +6,25 @@
 (setq wanted-packages
       ;; Auto-complete/ LSP / DAP packages
       '(lsp-mode
-	lsp-treemacs
-	dap-mode
-	flycheck
-	company
-	lsp-ui
-	hover
-	;; Dart packages
-	dart-mode
-	lsp-dart
-	;; Go Packages
-	go-mode
-	;; Ruby Packages
-	ruby-mode
-	;; Productivity
-	multiple-cursors	
-	magit
-	markdown-mode))
+				lsp-treemacs
+				dap-mode
+				flycheck
+				company
+				lsp-ui
+				hover
+				;; Container packages
+				dockerfile-mode
+				;; Dart packages
+				dart-mode
+				lsp-dart
+				;; Go Packages
+				go-mode
+				;; Ruby Packages
+				ruby-mode
+				;; Productivity
+				multiple-cursors
+				magit
+				markdown-mode))
 
 (when (cl-find-if-not #'package-installed-p wanted-packages)
   (package-refresh-contents)
